a2262272553f7d9ddf0ca04a45223cd001b86eb8
Stanley Wu <stanleywu@scrappylabs.dev>
2019-11-30 08:06:07 +0000

    add deltafetch extension

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -7,8 +7,14 @@ ROBOTSTXT_OBEY = True
 DOWNLOAD_DELAY = 1
 
 ITEM_PIPELINES = {
-    'scrapper.pipelines.FilePipeline': 100
+    'scrapper.pipelines.FilePipeline': 100,
+    'scrapper.pipelines.CSVPipeline': 1000
 }
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_deltafetch.DeltaFetch': 100,
+}
+DELTAFETCH_ENABLED = True
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware': 500
