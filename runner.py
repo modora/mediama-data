2d122ece237623a3cbd63fd2d4f402a08c378c73
Stanley Wu <stanleywu@scrappylabs.dev>
2019-11-30 08:23:52 +0000

    deltafetch dir changed to tmp and reset while debugging

--- runner.py
+++ runner.py
@@ -2,12 +2,21 @@ import sys
 
 from scrapy.cmdline import execute
 
+
 def main(spiders: list):
-	try:
-		execute(['scrapy', 'crawl'] + spiders)
-	except SystemExit:
-		pass
+    try:
+        (
+            execute(
+                ["scrapy", "crawl"]
+                + spiders
+                + ["-a", "deltafetch_reset=1"]  # args
+                + ["-s", "DELTAFETCH_DIR=/tmp/deltafetch"]
+            )  # settings
+        )
+    except SystemExit:
+        pass
+
 
 if __name__ == "__main__":
-	spiders = sys.argv[1:]
-	main(spiders)
+    spiders = sys.argv[1:]
+    main(spiders)
